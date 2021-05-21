cf54b08336bb3fb4a51e4a2ebd196f83082c1713
Keona Kirby <keonakirby@gmail.com>
2021-05-21 11:53:16 -0500

    updated seed and schema

--- models/pizza.js
+++ models/pizza.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
 const pizzaSchema = new Schema({
-  title: { type: String, required: true },
-  author: { type: String, required: true },
-  synopsis: String,
-  date: { type: Date, default: Date.now }
+  _id: { type: String, required: true },
+  type: { type: String, required: true },
+  toppings: { type: String, required: true },
+  size: { type: Number, required: true },
+  price: { type: Number, required: true },
+  gluten: Boolean,
+  date: { type: Date, default: Date.now },
 });
 
 const Pizza = mongoose.model("Pizza", pizzaSchema);
