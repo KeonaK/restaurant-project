49917f82cbbe3be88651b8f068d3691833611f67
Keona Kirby <keonakirby@gmail.com>
2021-05-24 09:12:21 -0500

    updated routes

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,21 +1,29 @@
 import React from "react";
 import "./App.css";
 import Nav from "../src/components/Nav/index";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import Landing from "../src/pages/Landing";
+import Ordering from "../src/pages/Ordering";
+import SignUp from "../src/pages/SignUp";
+import Login from "../src/pages/Login";
+import Checkout from "../src/pages/Checkout";
+import NoMatch from "../src/pages/NoMatch";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <Router>
       <div>
         <Nav />
-        <Route exact path="/" component={Landing} />
-        <Route exact path="/landing" component={Landing} />
-        <Route exact path="/restaurant-project" component={Landing} />
-        <Route exact path="/order" componenet={Ordering} />
-        <Route exact path="/sign-up" component={SignUp} />
-        <Route exact path="/login" component={Login} />
-        <Route exact path="/checkout" component={Checkout} />
-        <Route component={NoMatch} />
+        <Switch>
+          <Route exact path="/" component={Landing} />
+          <Route exact path="/landing" component={Landing} />
+          <Route exact path="/restaurant-project" component={Landing} />
+          <Route exact path="/order" componenet={Ordering} />
+          <Route exact path="/sign-up" component={SignUp} />
+          <Route exact path="/login" component={Login} />
+          <Route exact path="/checkout" component={Checkout} />
+          <Route component={NoMatch} />
+        </Switch>
 
         <Footer />
       </div>
