03319cd94a27d092c682f4b7234fd2a41b5c967d
Andrew Medina <andre.30001197@gmail.com>
2021-05-24 10:32:37 -0500

    landing page layout

---
+++ client/src/pages/Landing.js
@@ -0,0 +1,32 @@
+import React from "react";
+
+let Landing = React.createClass({
+    render: function() {
+      return (
+        <div>
+          function Landing(){'{'}
+          return (
+          <section classname="hero is-primary">
+            <div classname="hero-body">
+              <p className="title">
+                A La Leña
+              </p>
+              <p className="subtitle">
+                Pizzeria
+              </p>
+            </div>
+          </section>
+          <section className="section is-small">
+            <h1 className="title">Welcome to pizza heaven.</h1>
+            <h2 className="subtitle">
+             Create an account to order online.
+            </h2>
+          </section>
+          )
+          {'}'};
+        </div>
+      );
+    }
+  });
+
+  export default Landing;
